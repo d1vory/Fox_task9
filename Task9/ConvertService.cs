6fb1b524c46bad44f232a17e7b596e0d6f3cff0a
Olexandr Dubchak <a.dubchak@disoft.dev>
2024-05-07 20:36:43 +0300

    temp parse json

--- Task9/ConvertService.cs
+++ Task9/ConvertService.cs
@@ -1,21 +1,44 @@
+using Newtonsoft.Json;
+using JsonSerializer = System.Text.Json.JsonSerializer;
+
 namespace Task9;
 
 public class ConvertService
 {
-    public ConvertService(string foreignCurrency, decimal inputValue, DateOnly date)
+    public ConvertService(string foreignCurrency, DateOnly date)
     {
         ForeignCurrency = foreignCurrency;
-        InputValue = inputValue;
         Date = date;
     }
     
-    public decimal InputValue { get; }
     public DateOnly Date { get; }
     public string ForeignCurrency { get; }
 
-    public decimal GetUAHExchangeValue()
+    public class PBResponse
     {
+        [JsonProperty("exchangeRate")]
+        public List<ExchangeRate> ExchangeRates {get;set;}
+    }
+    public class ExchangeRate
+    {
+        [JsonProperty("currency")]
+        public string Currency {get;set;}
         
+        [JsonProperty("saleRateNB")]
+        public decimal SaleRate {get;set;}
+        
+        [JsonProperty("purchaseRateNB")]
+        public decimal PurchaseRate {get;set;}
     }
     
+    public async Task GetUAHExchangeValue()
+    {
+        using var client = new HttpClient();
+        var kek = await client.GetStringAsync("https://api.privatbank.ua/p24api/exchange_rates?date=01.12.2014");
+        
+        var stream = await client.GetStreamAsync("https://api.privatbank.ua/p24api/exchange_rates?date=01.12.2014");
+        var json =  JsonSerializer.Deserialize<PBResponse>(kek);
+        var json2 = JsonConvert.DeserializeObject<PBResponse>(kek);
+        Console.WriteLine(json);
+    }
 }
--- Task9/Program.cs
+++ Task9/Program.cs
@@ -4,8 +4,11 @@ class Program
 {
     static async Task Main()
     {
-        var bot = new Bot("7109512993:AAGt9rFAfqkVeN9OIdBKRXbmv2xCfWtiSzc");
-        await bot.Start();
+        // var bot = new Bot("7109512993:AAGt9rFAfqkVeN9OIdBKRXbmv2xCfWtiSzc");
+        // await bot.Start();
+
+        var cs = new ConvertService("USD", new DateOnly(2024, 2, 2));
+        await cs.GetUAHExchangeValue();
 
     }
 }
